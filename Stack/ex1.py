0a386d6fd5157a49a520532c3927fcedaa2da556
happyprakhar <87803283+happyprakhar@users.noreply.github.com>
2022-09-29 00:01:28 +0530

    Update ex1.py

--- Stack/ex1.py
+++ Stack/ex1.py
@@ -1,3 +1,4 @@
+# next greater elment in an array
 def fun(nums):
     # putting -1 on stack on stack to avoid empty problem
     # intializing res with -1
@@ -14,3 +15,7 @@ a=[1,3,4,6,7,2]
 b=[5,4,3,2,1]
 print(fun(a))
 print(fun(b))
+
+Output:
+[3, 4, 6, 7, -1, -1]
+[-1, -1, -1, -1, -1]
